8339985e793039f4a89a19f83c933584fd5085f4
Bugra <seyitbugraerden@gmail.com>
2024-02-01 15:03:27 +0300

    Header / Swiper / Images Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import logo from "../../assets/img/logo.png";
+import { Badge } from "primereact/badge";
 import "./Header.css";
 
 function Header() {
   return (
     <nav>
-      <div className="container py-5 d-flex justify-content-between w-100 align-items-center">
+      <div className="container py-2 d-flex justify-content-between w-100 align-items-center">
         <div className="navbar-image">
-          <img src="https://www.cers.com.tr/assets/img/cers-logo.svg" alt="" />
+          <img src={logo} alt="" />
         </div>
         <div className="navbar-link">
           <ul className="d-flex flex-row gap-5">
-            <li className="navbar-item">
-              <a href="">Home</a>
+            <li className="navbar-item pt-1">
+              <a href="">Ana Sayfa</a>
             </li>
             <li className="navbar-item">
-              <a href="">Order List</a>
+              <a href="">
+                <i className="bi bi-box-fill p-overlay-badge">
+                  <Badge value="5" severity="danger"></Badge>
+                </i>
+              </a>
             </li>
           </ul>
         </div>
--- src/components/Swiper/MySwiper.jsx
+++ src/components/Swiper/MySwiper.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
+import iphones from "../../assets/img/iphones.png";
+import "swiper/css/autoplay";
 import "./MySwiper.css";
 import "swiper/css/pagination";
 import "swiper/css";
+import "animate.css";
 
 function MySwiper() {
   return (
@@ -11,16 +14,37 @@ function MySwiper() {
       className="mySwiper"
       loop={true}
       pagination={true}
-      modules={[Pagination]}
+      autoplay={{ delay: 5000 }}
+      modules={[Autoplay, Pagination]}
     >
       <SwiperSlide>
+        <div className="container">
+          <h1 className="animate__animated animate__fadeInLeft">
+            Sınırları Kaldırın, Teknolojiyle Yenilikleri Keşfedin.
+          </h1>
+        </div>
         <img src="https://picsum.photos/1920/1080" alt="" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="https://picsum.photos/1920/1080" alt="" />
+        <div className="container">
+          <h1 className="animate__animated animate__fadeInLeft">
+            Geleceği Şekillendirin, Teknolojiyle Bir Adım Önde Olun.
+          </h1>
+          <img
+            className="animate__animated animate__fadeInRight"
+            src={iphones}
+            alt=""
+          />
+        </div>
+        <img src="https://picsum.photos/1920/1081" alt="" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="https://picsum.photos/1920/1080" alt="" />
+        <div className="container">
+          <h1 className="animate__animated animate__fadeInLeft">
+            Teknoloji: Hayal Gücünüzü Gerçeğe Dönüştürmenin Anahtarı.
+          </h1>
+        </div>
+        <img src="https://picsum.photos/1920/1082" alt="" />
       </SwiperSlide>
     </Swiper>
   );
