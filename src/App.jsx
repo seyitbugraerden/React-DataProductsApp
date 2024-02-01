dd6e60015be1d8ba81af9ae4fc5d7994c27c6785
Bugra <seyitbugraerden@gmail.com>
2024-02-01 15:25:00 +0300

    Product Item update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Swiper from "./components/Swiper/MySwiper";
+import ProductsList from "./components/ProductList/ProductsList";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
       <Swiper />
+      <main>
+        <ProductsList />
+      </main>
     </>
   );
 }
---
+++ src/components/ProductList/ProductsList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProductsList() {
+  return (
+    <section className="container">
+      <h2>Product List</h2>
+    </section>
+  );
+}
+
+export default ProductsList;
