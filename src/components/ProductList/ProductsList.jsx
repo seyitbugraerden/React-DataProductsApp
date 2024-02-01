7778da7caaf1f01b8b4800a2622f5df9a6aed6e1
Bugra <seyitbugraerden@gmail.com>
2024-02-01 20:36:34 +0300

    Filter Menu

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -9,30 +9,31 @@ import data from "../../data/data.json";
 
 function ProductsList() {
   const [products, setProducts] = useState(data);
-  const [search, setsearch] = useState("Apple");
-  const [brands, setbrands] = useState(["Apple", "Samsung"]);
+  const [search, setSearch] = useState("Apple");
+  const [brands, setBrands] = useState(["Apple", "Samsung"]);
+
+  useEffect(() => {
+    // Filter products based on selected brand
+    const filteredProducts = data.filter((item) => item.brand === search);
+    setProducts(filteredProducts);
+  }, [search]);
+
   return (
     <section className="container">
       <h2>Product List</h2>
       <Dropdown
-        value={setsearch("Samsung")}
+        value={search}
         options={brands}
-        optionLabel="name"
-        placeholder="Select a City"
+        onChange={(e) => setSearch(e.value)}
+        placeholder="Select a Brand"
         className="w-full md:w-14rem"
       />
       <Swiper slidesPerView={4} spaceBetween={30} className="mySwiper2">
-        {data.map((item, index) =>
-          item.brand === search ? (
-            <SwiperSlide key={index}>
-              <ProductItem item={item} />
-            </SwiperSlide>
-          ) : (
-            <SwiperSlide key={index}>
-              <ProductItem item={item} />
-            </SwiperSlide>
-          )
-        )}
+        {products.map((item, index) => (
+          <SwiperSlide key={index}>
+            <ProductItem item={item} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
