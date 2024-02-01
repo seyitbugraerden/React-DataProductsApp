25f23ace91d47394bcaba654e5338323b41d9133
Bugra <seyitbugraerden@gmail.com>
2024-02-01 20:35:51 +0300

    Select Box

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Dropdown } from "primereact/dropdown";
 import "./ProductsList.css";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -9,15 +9,30 @@ import data from "../../data/data.json";
 
 function ProductsList() {
   const [products, setProducts] = useState(data);
+  const [search, setsearch] = useState("Apple");
+  const [brands, setbrands] = useState(["Apple", "Samsung"]);
   return (
     <section className="container">
       <h2>Product List</h2>
+      <Dropdown
+        value={setsearch("Samsung")}
+        options={brands}
+        optionLabel="name"
+        placeholder="Select a City"
+        className="w-full md:w-14rem"
+      />
       <Swiper slidesPerView={4} spaceBetween={30} className="mySwiper2">
-        {data.map((item, index) => (
-          <SwiperSlide key={index}>
-            <ProductItem item={item} />
-          </SwiperSlide>
-        ))}
+        {data.map((item, index) =>
+          item.brand === search ? (
+            <SwiperSlide key={index}>
+              <ProductItem item={item} />
+            </SwiperSlide>
+          ) : (
+            <SwiperSlide key={index}>
+              <ProductItem item={item} />
+            </SwiperSlide>
+          )
+        )}
       </Swiper>
     </section>
   );
