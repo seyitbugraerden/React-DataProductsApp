ea1627a9f8cc38faa67ad91280d79567d27347df
Bugra <seyitbugraerden@gmail.com>
2024-02-01 16:03:14 +0300

    product List Updates

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "./ProductsList.css";
+import "swiper/css";
+import "swiper/css/pagination";
 
 function ProductsList() {
   return (
     <section className="container">
       <h2>Product List</h2>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper2"
+      >
+        <SwiperSlide>
+          <div className="card">
+            <img src="https://picsum.photos/400/400" alt="" />
+            <h4>İphone</h4>
+            <div className="text-muted">Apple</div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
